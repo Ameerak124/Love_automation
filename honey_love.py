e506f9c9e8d201eaa839799c714830e4f9e10a9e
SHEIK AMEER <58861089+Ameerak124@users.noreply.github.com>
2020-06-07 15:47:16 +0530

    Update honey_love.py

--- honey_love.py
+++ honey_love.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
-account_sid = 'AC09fc7a7c525d079ddbfd588d63fb2233' 
-auth_token = 'b0419ca52d723cb2429f272506f6816a' 
+account_sid = 'AC09fc7a7c525d079ddbfd588d0000' 
+auth_token = 'b0419ca52d723cb2429f272506f68000000' 
 client = Client(account_sid, auth_token) 
 def send_love():
     message = client.messages.create( 
